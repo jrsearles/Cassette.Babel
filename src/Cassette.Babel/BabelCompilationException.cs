ba0cbfbdab80809e12e2b342a34f3e8f75083f61
Joshua Searles <jsearles@plex.com>
2017-05-19 13:38:09 -0400

    Follow standard guidelines for exceptions

--- src/Cassette.Babel/BabelCompilationException.cs
+++ src/Cassette.Babel/BabelCompilationException.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Cassette.Babel
 {
   [Serializable]
   public class BabelCompilationException : Exception
   {
+    protected BabelCompilationException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
     public BabelCompilationException(string message, string path, Exception innerException) : base(message, innerException)
     {
       this.SourcePath = path;
